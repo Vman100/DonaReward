f6feee99ff02e88cf200227f17061f56be90865f
Vedran Tepavcevic <mr_vedran@hotmail.com>
2019-11-21 14:17:17 -0500

    Modify donate function and milestone multipler

--- DonaRewards.sol
+++ DonaRewards.sol
@@ -25,12 +25,13 @@ contract DonaRewards {
     
     uint256 public goal;
     uint256 public milestone;
-    bool public isMilestone;
+    uint256 public milestoneMultipler;
+    bool public isWithdrawEnabled;
     address public donaRewardsContractAddress = address(this);
     mapping(address => Contributor) public contributorsDB;
     uint256[] public donationBracket = [10, 50, 100, 500, 1000, 5000, 10000, 50000];
     
-    constructor(address _rewardsContract, uint256 _goal, uint256 _milestone) public {
+    constructor(address _rewardsContract, uint256 _goal, uint256 _milestone, uint256 _milestoneMultipler) public {
         require(_goal > 0, "goal amount must be greater then 0");
         require(_milestone > 0 && _milestone < _goal, "milestone amount must greater the 0 and less then goal amount");
         rewardsContract = rewardInterface(_rewardsContract);
@@ -39,6 +40,12 @@ contract DonaRewards {
         }
         goal = _goal;
         milestone = _milestone;
+        if(_milestoneMultipler == 0) {
+            milestoneMultipler = 1;
+        }else {
+            milestoneMultipler = _milestoneMultipler;
+        }
+        
     }
     
     modifier isAuthorized() {
@@ -59,16 +66,35 @@ contract DonaRewards {
     
     function donate() public isAuthorized payable {
         require(checkDonation(msg.value), "donation amount does not match any donation brackets");
+        
         contributorsDB[msg.sender].donationAmount += msg.value;
-        if(donaRewardsContractAddress.balance >= milestone) {
-             rewardsContract.trigger();
-             rewardsContract.withdraw();
-             isMilestone = true
+        if(donaRewardsContractAddress.balance == milestone) {
+            contributorsDB[msg.sender].prevPercentageAmount += msg.value * 10 / milestone;
+            rewardsContract.trigger();
+            rewardsContract.withdraw();
+            isWithdrawEnabled = true;
+        } else if(donaRewardsContractAddress.balance > milestone) {
+            contributorsDB[msg.sender].prevPercentageAmount += (msg.value - (donaRewardsContractAddress.balance - milestone)) * 10 / milestone;
+            rewardsContract.trigger();
+            rewardsContract.withdraw();
+            isWithdrawEnabled = true;
         }
-        if(isMilestone) {
+        if(isWithdrawEnabled) {
             contributorsDB[msg.sender].currentPercentageAmount += msg.value * 10 / milestone;
+            if(milestoneMultipler == 1) {
+                milestone += milestone;
+            } else {
+                milestone += milestone * milestoneMultipler;
+            }
+            if(milestone > goal) {
+                milestone = goal;
+            }
         } else {
-            contributorsDB[msg.sender].prevPercentageAmount = currentPercentageAmount
+            if(contributorsDB[msg.sender].currentPercentageAmount == 0) {
+                contributorsDB[msg.sender].prevPercentageAmount += msg.value * 10 / milestone;
+            } else {
+                contributorsDB[msg.sender].prevPercentageAmount = contributorsDB[msg.sender].currentPercentageAmount;
+            }
             contributorsDB[msg.sender].currentPercentageAmount += msg.value * 10 / milestone;
         }
     }
@@ -85,11 +111,14 @@ contract DonaRewards {
         uint256 amount = calculateRewards();
         require(amount >0);
         rewardsContract.transfer(msg.sender, amount);
+        if(balanceOf(donaRewardsContractAddress) == 0) {
+            isWithdrawEnabled = false;
+        }
     }
     
     function calculateRewards() private returns(uint256) {
         uint256 tokenBalance = rewardsContract.totalSupply();
-        contributorsDB[msg.sender].tokenAmount = tokenBalance * contributorsDB[msg.sender].currentPercentageAmount / 10;
+        contributorsDB[msg.sender].tokenAmount = tokenBalance * contributorsDB[msg.sender].prevPercentageAmount / 10;
         return contributorsDB[msg.sender].tokenAmount;
     }
     
